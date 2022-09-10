60b2dfeb31eaf12ea491545457951a1b5f086465
samsonkayajibade <samsonajibade16@gmail.com>
2022-09-10 12:43:49 -0700

    4.print alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
