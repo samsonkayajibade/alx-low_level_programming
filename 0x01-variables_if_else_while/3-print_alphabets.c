992e7cc67bc0f6544b9320357c157e279c1da463
samsonkayajibade <samsonajibade16@gmail.com>
2022-09-10 12:33:04 -0700

    3.print alhabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z') /*print lowercases a-z*/
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	while (upper <= 'Z') /*print uppercase A-Z*/
+	{
+		putchar(upper);
+		upper++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
