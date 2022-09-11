33e1745c326b2daff6bef1d7e50868b573469aa4
samsonkayajibade <samsonajibade16@gmail.com>
2022-09-11 13:26:37 -0700

    7.print alpga

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
-
 /**
-
  * main - Entry point
-
  * Return: 0
-
  */
-
 int main(void)
-
 {
-
 	char c = 'z';
 
-
-
 	while (c >= 'a')
-
 	{
-
 	putchar(c);
-
 	c--;
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
-
