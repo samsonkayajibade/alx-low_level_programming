282d173cc91d39b0042bce183f884cbd1ff4d7f6
samsonkayajibade <samsonajibade16@gmail.com>
2022-09-14 05:59:32 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z
+ * Return: 0
+ *
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
